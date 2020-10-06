94b768274b283d9487e99399f81d6817fd7f0064
Chris Co <coswarm@gmail.com>
2020-10-07 00:15:11 +0300

    fix Dropdown css

--- repos/web/stories/Dropdowns.js
+++ repos/web/stories/Dropdowns.js
@@ -13,26 +13,44 @@ export default class Dropdowns extends Component {
     const options = ['Alpha', 'Beta', 'Mega']
     return (
       <View className='full-width'>
-
-
-        <Row className='full-width center wrap margin'>
+        <Row className='full-width center wrap margin top'>
           {/* Raw Inputs */}
-          <form className='app__form full-width max-width-290 padding-h-small'>
-            <Dropdown placeholder='Select' float options={options}/>
-            <Dropdown placeholder='Disabled' disabled float options={options}/>
-            <Dropdown placeholder='Disabled' label='disabled' value={options[0]} disabled float options={options}/>
-            <Dropdown placeholder='Loading...' loading float options={options}/>
-          </form>
-          {/* Raw Inputs */}
-          <form className='app__form full-width max-width-290 padding-h-small'>
-            <Dropdown placeholder='Search' search float options={options}/>
-            <Dropdown placeholder='Readonly' readonly float options={options}/>
-            <Dropdown placeholder='Readonly' label='readonly' value={options[0]} readonly float options={options}/>
-            <Dropdown placeholder='Loading...' label='done' value={[1]} loading search float options={[
+          <View className='app__form full-width max-width-290 padding-h-small'>
+            {/* @Note: float label only works correctly with controlled value */}
+            <Dropdown float label='Select' options={options}/>
+            <Dropdown placeholder='Select' options={options}/>
+            <Dropdown label='Select' placeholder='Enter...' options={options}/>
+
+            <Dropdown float label='Multiple' multiple options={options}/>
+            <Dropdown placeholder='Multiple' multiple options={options}/>
+            <Dropdown label='Multiple' placeholder='Enter...' multiple options={options}/>
+
+            <Dropdown float label='disabled' disabled value={options[0]} options={options}/>
+            <Dropdown placeholder='Disabled placeholder' disabled options={options}/>
+            <Dropdown label='disabled' placeholder='Enter...' disabled value={options[0]} options={options}/>
+
+            <Dropdown float label='Loading...' loading options={options}/>
+            <Dropdown placeholder='Loading search...' loading search value={[1]} options={[
               {text: '[1]', value: '1'},
               {text: '[2]', value: '2'},
             ]}/>
-          </form>
+            <Dropdown label='Loading...' placeholder='Enter...' loading options={options}/>
+          </View>
+
+          {/* Raw Inputs */}
+          <View className='app__form full-width max-width-290 padding-h-small'>
+            <Dropdown float label='Search' search options={options}/>
+            <Dropdown placeholder='Search' search options={options}/>
+            <Dropdown label='Search' placeholder='Enter...' search options={options}/>
+
+            <Dropdown float label='Search Multiple' search multiple options={options}/>
+            <Dropdown placeholder='Search Multiple' search multiple options={options}/>
+            <Dropdown label='Search Multiple' placeholder='Enter...' search multiple options={options}/>
+
+            <Dropdown float label='readonly' readonly value={options[0]} options={options}/>
+            <Dropdown placeholder='Readonly placeholder' readonly options={options}/>
+            <Dropdown label='readonly' placeholder='Enter...' readonly value={options[0]} options={options}/>
+          </View>
 
           {/* Redux Form Inputs */}
           {/*<form className='app__form full-width max-width-290 padding-h-small'>*/}
